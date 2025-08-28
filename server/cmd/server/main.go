5d2a76889581637834c3e7b963fe5f1f6773f019
ad <hi@arnaudiaz.com>
2025-08-28 15:42:25 +0200

    fixes lint

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -27,9 +27,9 @@ func main() {
 	router.Handle("/baz", telemetry.NewHandler(handlers.NewServerHandler("baz"), "ServerHandlerBaz"))
 
 	server := &http.Server{
-		Addr:        ":8080",
-		Handler:     router,
-		ReadTimeout: 5 * time.Second,
+		Addr:              ":8080",
+		Handler:           router,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 
 	go func() {
